0fca2523547deffdcda8a9dc1f781128f55921f3
C4rL0sML <moralescarlosgustavo3@gmail.com>
2024-02-22 09:37:35 -0300

    Repo A

--- Assets/Scrips/index.js
+++ Assets/Scrips/index.js
@@ -7,17 +7,9 @@ let tituloPrincipal = "Frutas"
 titulo.innerText = tituloPrincipal
 
 //E3
-// const gris = document.getElementsByClassName('orange')
-// console.log(gris)
-
-// gris.style.property = Text-withe
-
-
-// gris.className = " "
 
 //E4
 let pie = document.getElementsByName('p')
 console.log(pie)
-
 
 //E5
